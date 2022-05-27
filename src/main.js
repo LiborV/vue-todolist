c23793c9594fe294f0d961b013393100305093ae
Libor Vymětalík <libor.vymetalik@nic.cz>
2022-05-27 14:55:01 +0200

    Set axios.defaults.baseURL

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import VueAxios from 'vue-axios'
 import bootstrap from 'bootstrap'
 
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'http://localhost:3000/api/v1/'
+axios.defaults.baseURL = process.env.VUE_APP_SERVICE_URL
 // axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie.split('=')[1]}`
 
 createApp(App).use(store).use(router).use(VueAxios, axios).mount('#app')
